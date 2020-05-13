9523d56bf4170ed9d04543c6238657c8495c48f4
R.S. Knox <rsknox@gmail.com>
2020-05-13 13:41:19 -0500

    added second LED blink logic and added logging to .log file

--- Blink_threaded.py
+++ Blink_threaded.py
@@ -3,9 +3,14 @@
 from gpiozero import LED
 from time import sleep
 import time
+import logging
+logging.basicConfig(filename='blink.log', level=logging.INFO)
 
-led = LED(15)
-led.off()
+
+led15 = LED(15)
+led18 = LED(18)
+led15.off()
+led18.off()
 def led_on():
     led.on()
     print("led on")
@@ -14,17 +19,65 @@ def led_off():
     led.off()
     print("led off")
 
-led_on_state = False
-d_start = time.time()
-d1 = .1 # lenth of time the LED is off
-d2 = .5  # length of time the LED is on
+led15_on_state = False
+d15_start = time.time()
+d151 = 1.0 # lenth of time the LED is off
+d152 = 1.0  # length of time the LED is on
+
+led18_on_state = False
+d18_start = time.time()
+d181 = .6 # lenth of time the LED is off
+d182 = .1  # length of time the LED is on
+t = int(round(time.time()*1.000))
+logging.info("Start at {a}".format (a=t))
 while True:
-    if not led_on_state and time.time() > (d_start + d1):
-        led.on()
-        led_on_state = True
-    else:
-        if led_on_state and time.time() > (d_start + d2 + d1):
-            led.off()
-            led_on_state = False
-            d_start = time.time()
+#     if not led15_on_state and time.time() > (d15_start + d151):
+#         led15.on()
+#         led15_on_state = True
+#     else:
+#         if led15_on_state and time.time() > (d15_start + d152 + d151):
+#             led15.off()
+#             led15_on_state = False
+#             d15_start = time.time()
+#             
+#     if not led18_on_state and time.time() > (d18_start + d181):
+#         led18.on()
+#         led18_on_state = True
+#     else:
+#         if led18_on_state and time.time() > (d18_start + d182 + d181):
+#             led18.off()
+#             led18_on_state = False
+#             d18_start = time.time()
+
+    t = int(round(time.time()*1.000))
+#    logging.info("Top of Loop --- {a}".format (a=t))
+
+    
+
+    if not led15_on_state and time.time() > (d15_start + d151):
+        led15.on()
+        led15_on_state = True
+        t = int(round(time.time()*1.000))
+        logging.info("Point A {a}".format (a=t))
+
+    if led15_on_state and time.time() > (d15_start + d152 + d151):
+        led15.off()
+        led15_on_state = False
+        d15_start = time.time()
+        t = int(round(time.time()*1.000))
+        logging.info("Point B {a}".format (a=t))
+        
             
+    if not led18_on_state and time.time() > (d18_start + d181):
+        led18.on()
+        led18_on_state = True
+        t = int(round(time.time()*1.000))
+        logging.info("Point C {a}".format (a=t))
+        
+        
+    if led18_on_state and time.time() > (d18_start + d182 + d181):
+        led18.off()
+        led18_on_state = False
+        d18_start = time.time()
+        t = int(round(time.time()*1.000))
+        logging.info("Point D {a}".format (a=t))    
