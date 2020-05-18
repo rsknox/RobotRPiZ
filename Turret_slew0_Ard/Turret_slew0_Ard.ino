879f166e1a4a310eb747652c448f9c32988c73b7
rsknox <rsknox@gmail.com>
2020-05-18 12:08:23 -0500

    got Turret_slew0_Ard operational, will receive block from RPi and return one byte with numbytes received

--- Turret_slew0_Ard/Turret_slew0_Ard.ino
+++ Turret_slew0_Ard/Turret_slew0_Ard.ino
@@ -8,6 +8,7 @@
 #define LED_PIN 13
 boolean ledon = HIGH;
 byte slave_address = 7;
+int echonum = 0;    // added
 
 void setup() {
 //  // set up the LCD's number of columns and rows:
@@ -20,14 +21,21 @@ void setup() {
 //  
   Serial.begin(9600);      // open the serial port at 9600 bps: 
   Serial.print("Arduino Nano");
+  Serial.print('\n');
   Wire.begin(slave_address);
   Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);   // added
 }
 
 void loop() {
 
 }
 
+void requestEvent(){    // added
+
+  Wire.write(echonum);  // added
+  toggleLED();          // added
+}
 void receiveEvent(int howMany) {
 //  lcd.clear();
   
@@ -41,6 +49,7 @@ void receiveEvent(int howMany) {
   Serial.print("len:");
   Serial.print(numOfBytes);
   Serial.print(" "); 
+  Serial.print('\n');
   byte b = Wire.read();  //cmd
 //  lcd.print("cmd:");
 //  lcd.print(b);
@@ -48,6 +57,7 @@ void receiveEvent(int howMany) {
   Serial.print("cmd:");
   Serial.print(b);
   Serial.print(" ");
+  Serial.print('\n');
 
   //display message received, as char
 //  lcd.setCursor(0, 1);
@@ -57,7 +67,9 @@ void receiveEvent(int howMany) {
     Serial.print(data);
   }
   
-  toggleLED();
+//  toggleLED();
+  echonum = numOfBytes-1;   // added
+
 }
 
 void toggleLED(){
